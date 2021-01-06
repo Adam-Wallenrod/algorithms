868427a1e71f2239aeb7a8a0265492e535960f76
Adam-Wallenrod <psredniawa@gmail.com>
2021-01-06 16:46:19 +0100

    Finish Merge Sort

--- src/app/sorting/pages/merge-sort-page/merge-sort-page.component.ts
+++ src/app/sorting/pages/merge-sort-page/merge-sort-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-merge-sort-page',
@@ -7,56 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MergeSortPageComponent implements OnInit {
 
-  arrayToSort: number[] = [38, 27, 43, 3, 9, 82, 10];
+  // arrayToSort: number[] = [38, 27, 43, 3, 9, 82, 10];
+  arrayToSort: number[] = [38, 27, 15 , 43, 3, 9, 82, 15, 10, 38];
+  sortedArray: number[];
 
 
-
-
-
-  constructor() { }
-
-  ngOnInit() {
-    this.mergeSort(this.arrayToSort, 0, this.arrayToSort.length - 1);
+  constructor() {
   }
 
-  mergeSort(toSort: number[], leftIndex: number, rightIndex: number) {
+  ngOnInit() {
+    this.sortedArray = this.mergeSort(this.arrayToSort, 0, this.arrayToSort.length - 1);
+  }
+
+  mergeSort(toSort: number[], leftIndex: number, rightIndex: number): number[] {
     if (toSort.length === 1) {
       return toSort;
     }
 
     if (leftIndex < rightIndex) {
       const middleIndex: number = Math.round(leftIndex + (rightIndex - 1) / 2);
-      const firstHalf: number[] = toSort.slice(0, (middleIndex + 1) );
+      const firstHalf: number[] = toSort.slice(0, (middleIndex + 1));
       const secondHalf: number[] = toSort.slice(middleIndex + 1, toSort.length + 1);
 
-      const firstArray = this.mergeSort( firstHalf,  0, middleIndex);
-      const secondArray = this.mergeSort( secondHalf, middleIndex + 1, toSort.length );
+      const firstArray = this.mergeSort(firstHalf, 0, middleIndex);
+      const secondArray = this.mergeSort(secondHalf, 0, secondHalf.length - 1);
 
-      this.merge(firstHalf, secondHalf);
-
-
-
+      const sorted: number[] = this.merge(firstArray, secondArray);
+      return sorted;
     }
 
 
   }
 
 
-  merge(first: number[], second: number[]) {
+  merge(first: number[], second: number[]): number[] {
+    const newMergedArray: number[] = [];
 
-    let i: number = 0;
-    let newMergedArray: number[];
-
-    while( i < first.length) {
-
-      if() {
-
+    while (first.length && second.length) {
+      if (first[0] < second[0]) {
+        newMergedArray.push(first.shift());
+      } else {
+        newMergedArray.push(second.shift());
       }
-      i++;
 
     }
 
+    while (first.length) {
+      newMergedArray.push(first.shift());
+    }
+
+    while (second.length) {
+      newMergedArray.push(second.shift());
+    }
+
+    console.log('newMergedArray: ', newMergedArray);
+    return newMergedArray;
+
 
   }
+
 
 }
