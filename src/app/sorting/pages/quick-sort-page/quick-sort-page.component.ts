323fa74e9a5e413e7215534a9e1a0941d3934a8c
Adam-Wallenrod <psredniawa@gmail.com>
2021-01-07 22:35:39 +0100

    Working on Quick Sort

--- src/app/sorting/pages/quick-sort-page/quick-sort-page.component.ts
+++ src/app/sorting/pages/quick-sort-page/quick-sort-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-quick-sort-page',
@@ -7,9 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuickSortPageComponent implements OnInit {
 
-  constructor() { }
+  arrayToSort: number[] = [7, 2, 1, 4, 8, 6, 3, 5];
+  sortedArray: number[];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.sortedArray =  this.quickSort(this.arrayToSort);
+  }
+
+  quickSort(toSort: number[]): number[] {
+    const arrayCopy: number[] = toSort.slice();
+    const pivotIndex: number = Math.floor((toSort.length - 1) / 2);
+    const pivot: number = toSort[pivotIndex];
+    arrayCopy.splice(pivotIndex, 1);
+
+
+    console.log('pivotIndex: ', pivotIndex);
+    console.log('pivot: ', pivot);
+    // console.log('array without pivot: ', arrayCopy);
+
+    let i = -1;
+    let j = 0;
+
+    while (j < arrayCopy.length) {
+      if (arrayCopy[j] > pivot) {
+        j++;
+      } else if (arrayCopy[j] < pivot) {
+        let temp: number;
+        i++;
+        temp = arrayCopy[j];
+        arrayCopy[j] = arrayCopy[i];
+        arrayCopy[i] = temp;
+        j++;
+      }
+
+
+    }
+
+    console.log('arrayCopy: ', arrayCopy);
+
+    const leftSide = arrayCopy.slice(0, (i + 1));
+    const rightSide = arrayCopy.slice((i + 1), arrayCopy.length);
+    console.log('leftSide: ', leftSide);
+    console.log('rightSide: ', rightSide);
+
+    let leftSideSorted: number[];
+    let rightSideSorted: number[];
+
+    if (leftSide.length > 2) {
+      leftSideSorted = this.quickSort(leftSide);
+    } else {
+      leftSideSorted = leftSide;
+    }
+
+
+    if (rightSide.length > 2) {
+      rightSideSorted = this.quickSort(rightSide);
+    } else {
+      rightSideSorted = rightSide;
+    }
+
+    // leftSideSorted = this.quickSort(leftSide);
+    // rightSideSorted = this.quickSort(rightSide);
+
+    const result = leftSideSorted.concat(rightSideSorted);
+    result.splice(i, 0, pivot);
+    return result;
+
   }
 
 }
