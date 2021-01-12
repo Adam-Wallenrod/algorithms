3de21fddfa04737ebe05b3cf253d7b52b218eedf
Adam-Wallenrod <psredniawa@gmail.com>
2021-01-12 23:03:27 +0100

    Create MaxHeap based on elements in array to sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sorting-app';
+
+  constructor() {
+  }
+
+
+  ngOnInit() {
+    this.createArrays();
+  }
+
+
+  /**
+   * Different way of converting to array
+   */
+  createArrays() {
+    const listElements = document.documentElement.querySelectorAll('li');
+    console.log('listElements: ', listElements);
+
+    // first way:
+    const arr1 = Array.from(listElements);
+    console.log('Array.from() ---> arr2: ', arr1);
+
+    // second way :
+    const arr2 = [].slice.call(listElements);
+    console.log('[].slice.call() --> ', arr2);
+  }
+
+
 }
--- src/app/sorting/pages/heap-sort-page/heap-sort-page.component.ts
+++ src/app/sorting/pages/heap-sort-page/heap-sort-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-heap-sort-page',
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeapSortPageComponent implements OnInit {
 
-  constructor() { }
+  arrayToSort: number[] = [7, 10, 5, 22, 12, 17, 15, 11, 6, 9];
+  sortedArray: number[] = [];
+
+  constructor() {
+  }
+
 
   ngOnInit() {
+    this.createHeapTree(this.arrayToSort);
+  }
+
+
+  createHeapTree(inputArray: number[]) {
+    // create a copy
+    const copiedArray: number[] = inputArray.slice();
+    const maxHeap: number[] = [];
+
+    for (const [index, item] of copiedArray.entries()) {
+      maxHeap[index] = item;
+      this.trickleUp(maxHeap, item, maxHeap.length - 1);
+    }
+
+    console.log('maxHeap: ', maxHeap);
+  }
+
+
+  trickleUp(heap: number[], child: number, childIndex: number) {
+    const parentIndex = Math.floor((childIndex - 1) / 2);
+    if (childIndex !== 0 && heap[parentIndex] < child) {
+      this.swap(heap, parentIndex, childIndex);
+      this.trickleUp(heap, heap[parentIndex], parentIndex);
+
+    }
+
+
+  }
+
+
+  swap(arrayToModify: number[], i: number, j: number) {
+    const tmp = arrayToModify[j];
+    arrayToModify[j] = arrayToModify[i];
+    arrayToModify[i] = tmp;
   }
 
 }
