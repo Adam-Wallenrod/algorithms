e2aa9549365a35266be6b01bd0f56ef977924b86
Adam-Wallenrod <psredniawa@gmail.com>
2021-02-14 18:46:23 +0100

    Cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Queue} from './data-structures/queue/queue';
-import {Stack} from './data-structures/stack/stack';
-// import {Add} from './data-structures/queue/undo';
-import {Add, OpsStack, Times} from './data-structures/queue/undo2';
-import {Tree} from '@angular/router/src/utils/tree';
-import {BinarySearchTree} from './data-structures/tree/BST';
-import {Graph} from './data-structures/graph/graph';
-import {AnyList} from './generic-types/generic-types';
-import {User} from './generic-types/user';
+
 
 @Component({
   selector: 'app-root',
@@ -16,8 +8,7 @@ import {User} from './generic-types/user';
 })
 export class AppComponent implements OnInit {
   title = 'sorting-app';
-  // addOperations = new Add(2);
-  // numberToAdd: number;
+
 
   constructor() {
   }
@@ -31,18 +22,6 @@ export class AppComponent implements OnInit {
     console.log('[1]: ', myFirstTuple[1]);
     console.log('[2]: ', myFirstTuple[2]);
 
-    /***************************************************/
-    // const userList: AnyList = AnyList.create([new User('George'), new User('Jerry') ]);
-    // userList.add(new User('Elaine'));
-    // // userList.add(145);
-    // // userList.add('what am I even doing here?');
-    // console.log('userList: ', userList);
-    // const filteredUser: AnyList = userList.filter( (user) => user.getName() === 'Jerry');
-    // console.log('filteredUser: ', filteredUser);
-    // const seinfeldCast: AnyList = userList.select((user) => 'Seinfeld star: ' + user.getName());
-    // console.log('seinfeldCast: ', seinfeldCast);
-    // console.log(seinfeldCast === userList);
-
 
     console.log(this.identity<string>('zzz'));
     console.log(this.identityArrow<number[]>([1000000, 2000000, 3000000]));
@@ -50,18 +29,17 @@ export class AppComponent implements OnInit {
   }
 
 
-  // addNumber(value: number) {
-  //   console.log(`Number to add:  ${this.numberToAdd}`);
-  //   this.addOperations.add(value);
-  // }
-  //
-  // undoAdd() {
-  //   this.addOperations.undo();
-  // }
-  //
-  // getAddValue() {
-  //   return this.addOperations.getValue();
-  // }
+  /**
+   * An example of the simplest method which
+   * is using Generic Types.
+   *
+   * @param input
+   */
+  identity<T>(input: T): T {
+    return input;
+  }
+
+  identityArrow = <T>(input: T): T => input;
 
 
   /**
@@ -97,19 +75,6 @@ export class AppComponent implements OnInit {
     console.log('result: ', result);
 
   }
-
-
-  /**
-   * An example of the simplest method which
-   * is using Generic Types.
-   *
-   * @param input
-   */
-  identity<T>(input: T): T {
-    return input;
-  }
-
-  identityArrow = <T>(input: T): T => input;
 
 
 }
